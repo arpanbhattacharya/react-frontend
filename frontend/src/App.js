1bc3b1593dba1083428aeb4e5e4e2091d4ab24bd
Arpan Bhattacharya <112757502+arpanbhattacharya@users.noreply.github.com>
2024-02-23 13:02:49 +0000

    add feature 2nd page for showing image and design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Image from './pages/Image';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/image' element={<Image />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Image.jsx
@@ -0,0 +1,14 @@
+export default function Image() {
+  return (
+    <>
+      <div className="container">
+        <div className="main">
+          <div className="image">
+            <p>The image will go here</p>
+          </div>
+          <button id="btn-show">Show image</button>
+        </div>
+      </div>
+    </>
+  );
+}
