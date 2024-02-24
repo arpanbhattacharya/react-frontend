6085c4f0018de2229d891f293e201a2098987a40
Arpan Bhattacharya <112757502+arpanbhattacharya@users.noreply.github.com>
2024-02-24 07:56:30 +0000

    Create full screen toggle button in Image.jsx

--- frontend/src/pages/Image.jsx
+++ frontend/src/pages/Image.jsx
@@ -1,12 +1,26 @@
+import { useRef } from "react";
+
 export default function Image() {
+  const element = useRef(null);
+
+  const toggleFullScreen = () => {
+    const isFullScreen = document.fullscreenElement;
+
+    if (!isFullScreen) {
+      element.current.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  };
   return (
     <>
       <div className="container">
         <div className="main">
-          <div className="image">
-            <p>The image will go here</p>
+          <div className="image" ref={element}>
+            <button id="fullscreen-btn" onClick={toggleFullScreen}>
+              toggle
+            </button>
           </div>
-          <button id="btn-show">Show image</button>
         </div>
       </div>
     </>
